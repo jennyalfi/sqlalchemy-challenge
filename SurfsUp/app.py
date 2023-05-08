db23b600e94de62c6816583cb8429b2aa351c243
jennyalfi <jenalfson@gmail.com>
2023-05-08 15:34:36 -0700

    5.8 update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -10,38 +10,6 @@ from sqlalchemy import create_engine, func
 import datetime as dt
 
 
-
-# List all the available routes.
-#Define what to do when a user hits the route
-# @app.route("/")
-# def home():
-#     print("Server received request for 'Home' page...")
-#     return "Welcome to my 'Home' page!"
-
-# @app.route("/about")
-# def about():
-#     name = "Jen"
-#     location = "California"
-
-#     return f"My name is {name}, and I live in {location}."
-
-# @app.route("/contact")
-# def contact():
-#     email = "jen@fakeemail.com"
-
-#     return f"Questions? Comments? Please send an email to {email}."
-
-
-
-
-# 2.) API precipitation
-# Convert the query results from your precipitation analysis 
-# (i.e. retrieve only the last 12 months of data) to a dictionary 
-# using date as the key and prcp as the value.
-
-
-# Return the JSON representation of your dictionary.
-
 #################################################
 # Database Setup
 #################################################
@@ -61,11 +29,11 @@ station = base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
+
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
 
 
 #################################################
@@ -77,14 +45,10 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/station<br/>"
-        f"/api/v1.0/date<br/>"
-        f"/api/v1.0/prcp<br/>"
+        f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/temp/yyyy-mm-dd<br/>"
         f"/api/v1.0/temp/start/end<br/>"
-        f"/api/v1.0/longitude<br/>"
-        f"/api/v1.0/elevation<br/>"
     )
 
 
@@ -95,11 +59,10 @@ def welcome():
 
 # Return the JSON representation of your dictionary.
 
-
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
-    """Return a list of precipitation data including the name, age, and sex of each passenger"""
+    """Return a list of precipitation data for 8/23/2017 through 8/23/2017"""
     # Query all precipitation
     last_year = dt.date(2017,8,23)-dt.timedelta(days=365)
 
@@ -108,7 +71,7 @@ def precipitation():
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
+    # Create a dictionary from the row data and append to a list of all_precipitation
     all_precipitation = []
     for date, prcp in results:
         precipitation_dict = {}
@@ -120,39 +83,52 @@ def precipitation():
 
 
 
-
 # 3.) API stations
 # Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
 def stations():
 
-    """Return a list of precipitation data including the name, age, and sex of each passenger"""
-    # Query all precipitation
-
+    """Return a list of stations"""
+    # Query all stations
     results = session.query(station.station).all()
 
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    # all_precipitation = []
-    # for date, prcp in results:
-    #     precipitation_dict = {}
-    #     precipitation_dict["date"] = date
-    #     precipitation_dict["precipitation"] = prcp
-    #     all_precipitation.append(precipitation_dict)
+ 
     stations = list(np.ravel(results))
 
     return jsonify(stations)
 
 
-# 4.) API tobs (like one did but add filte for station)
+# 4.) API tobs (like one did but add filter for station)
 # Query the dates and temperature observations of the 
-# #most-active station for the previous year of data.
+# most-active station for the previous year of data.
 
 # Return a JSON list of temperature observations for the previous year.
 
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    """Return a list of temperature data of station USC00519281 for 8/23/2016 through 8/23/2017"""
+    # Query all temperature
+    last_year = dt.date(2017,8,23)-dt.timedelta(days=365)
+
+    results = session.query(measurement.date, measurement.tobs).filter(measurement.date >= last_year).filter(measurement.station == 'USC00519281').all()
+
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_temperature
+    all_temperature = []
+    for date, tobs in results:
+        temperature_dict = {}
+        temperature_dict["date"] = date
+        temperature_dict["temperature"] = tobs
+        all_temperature.append(temperature_dict)
+
+    return jsonify(all_temperature)
 
 
 # 5.) API start and end
@@ -160,14 +136,14 @@ def stations():
 # and the maximum temperature for a specified start or start-end range.
 
 # For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
-
 #For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 
 def temp(start=None,end=None):
 
-    """Return a list of temperature data including the name, age, and sex of each passenger"""
+    """Return a list of temperature data for specified start or start-end range"""
     sel = [func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)]
     if not end:
         results = session.query(*sel).filter(measurement.date >= start).all()
@@ -180,7 +156,6 @@ def temp(start=None,end=None):
     temps = list(np.ravel(results))
     return jsonify (temps)
    
-
 
 
 
